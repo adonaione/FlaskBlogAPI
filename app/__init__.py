1c1549a4856d739c970edccb96fd837bafeda48b
Adonai Romero <adonai@adonais-mbp.lan>
2024-04-17 13:40:06 -0600

    add comments to post dict, add flask-core

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask # Import the Flask class from the flask module
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS # Import CORS to allow Cross Origin Resource Sharing
 from config import Config
 
 # Create an instance of Flask called app which will be the central object
 app = Flask(__name__)
-#set the configuration for the app
+# Set the configuration for the app
 app.config.from_object(Config)
 
-# create an instance of SQLAlchemy called db which will be the central object for our dtabase
+# Allow Cross Origin Resource Sharing for all domains on all routes
+CORS(app)
+
+# Create an instance of SQLAlchemy called db which be the cental object for our database
 db = SQLAlchemy(app)
-#set the configuration for the app
+# Create an instance of Migrate with the app and db
 migrate = Migrate(app, db)
 
 # import the routes to the app and also the models
--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     posts = db.relationship('Post', back_populates='author')
-    comments = db.relationship('Comment', backref='user')
+    comments = db.relationship('Comment', back_populates='user')
     token = db.Column(db.String, index=True, unique=True)
     token_expiration = db.Column(db.DateTime(timezone=True))
 
@@ -34,7 +34,7 @@ class User(db.Model):
 
     def check_password(self, plaintext_password):
         return check_password_hash(self.password, plaintext_password)
-    
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -44,7 +44,7 @@ class User(db.Model):
             "email": self.email,
             "dateCreated": self.date_created
         }
-    
+
     def get_token(self):
         now = datetime.now(timezone.utc)
         if self.token and self.token_expiration > now + timedelta(minutes=1):
@@ -53,18 +53,18 @@ class User(db.Model):
         self.token_expiration = now + timedelta(hours=1)
         self.save()
         return {"token": self.token, "tokenExpiration": self.token_expiration}
-    
+        
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=lambda:datetime.now(timezone.utc))
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     # In SQL - user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     author = db.relationship('User', back_populates='posts')
-    comments = db.relationship('Comment', backref='post')
+    comments = db.relationship('Comment', back_populates='post')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -78,19 +78,19 @@ class Post(db.Model):
         db.session.commit()
 
     def delete(self):
-        db.session.delete(self) # deleting THIS object from the database
-        db.session.commit() # commiting our changes
+        db.session.delete(self)
+        db.session.commit()
 
     def to_dict(self):
         return {
-            "id" : self.id, 
+            "id": self.id,
             "title": self.title,
             "body": self.body,
             "dateCreated": self.date_created,
             "author": self.author.to_dict(),
-            'comments': [comment.to_dict() for comment in self.comment]
+            "comments": [comment.to_dict() for comment in self.comments]
         }
-    
+
     def update(self, **kwargs):
         allowed_fields = {'title', 'body'}
 
@@ -99,14 +99,18 @@ class Post(db.Model):
                 setattr(self, key, value)
         self.save()
 
+
 # Create our Comment class/table
 class Comment(db.Model):
     # CREATE TABLE
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=lambda:datetime.now(timezone.utc))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user = db.relationship('User',back_populates='comments')
+    post = db.relationship('Post',back_populates='comments')
+
 
     # INSERT INTO
     def __init__(self, **kwargs):
@@ -125,10 +129,10 @@ class Comment(db.Model):
         db.session.commit()
 
     def to_dict(self):
-        return{
+        return {
             'id': self.id,
             'body': self.body,
-            'dateCreated': self.date_created,  
+            'dateCreated': self.date_created,
             'post_id': self.post_id,
-            'user': self.user.to_dict()  # This will call the User's `to_dict()` method and include their info
+            'user': self.user.to_dict()
         }
